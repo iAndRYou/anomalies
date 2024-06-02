368f82cf57cbb376094a35067aafdae2af7f237b
iAndRYou <nawet3razy@gmail.com>
2024-06-02 21:27:51 +0200

    merged outliers

--- data_outliers.py
+++ data_outliers.py
@@ -1,10 +1,10 @@
 import numpy as np
+import pandas as pd
 from scipy import stats
-from pandas import DataFrame
 
-def get_mahalanobis_outliers(df: DataFrame, std_threshold: float = 4) -> DataFrame:
+def get_mahalanobis_outliers(df: pd.DataFrame, std_threshold: float = 4) -> pd.DataFrame:
     '''
-    Get the outliers in the data
+    Get the mahalanobis outliers in the data
     '''
     df = df.copy()
     mean = df['MD'].mean()
@@ -17,9 +17,9 @@ def get_mahalanobis_outliers(df: DataFrame, std_threshold: float = 4) -> DataFra
     
     return outliers
 
-def get_correlation_outliers(df: DataFrame, data_column: str, std_threshold: float = 4) -> DataFrame:
+def get_correlation_outliers(df: pd.DataFrame, data_column: str, std_threshold: float = 4) -> pd.DataFrame:
     '''
-    Get the outliers in the data
+    Get the correlation outliers in the data
     '''
     df = df.copy()
     slope, intercept, _, _, _ = stats.linregress(df['close'], df[data_column])
@@ -36,3 +36,15 @@ def get_correlation_outliers(df: DataFrame, data_column: str, std_threshold: flo
     outliers = df[np.abs(df['residuals']) > threshold]
     
     return outliers
+
+def get_merged_outliers(df: pd.DataFrame, data_column: str, std_threshold_mahalanobis: float = 4, std_threshold_correlation: float = 4) -> pd.DataFrame:
+    '''
+    Get the merged outliers for mahaanobis and given orrelation outliers in the data
+    '''
+    mahalanobis_outliers = get_mahalanobis_outliers(df, std_threshold_mahalanobis)
+    correlation_outliers = get_correlation_outliers(df, data_column, std_threshold_correlation)
+    
+    # Get the intersection of the two outliers sets
+    anomalies_index = pd.merge(mahalanobis_outliers, correlation_outliers, left_index=True, right_index=True, how='inner').index
+    
+    return df.loc[anomalies_index]
